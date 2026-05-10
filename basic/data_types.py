62d159e09f0cc6f894932b195f37f2589f04ecfb
Joinal Abden <joinalzx@gmail.com>
2026-05-10 19:14:05 +0600

    python recap basic

---
+++ basic/data_types.py
@@ -0,0 +1,12 @@
+import sys
+a = 10
+b = "python"
+# print(type(a))
+# print(type(b))
+
+a = "python"
+print(a)
+
+c = 500
+print(sys.getsizeof(c))
+
---
+++ basic/welcome.py
@@ -0,0 +1,10 @@
+# name = input("What is your name?")
+# print("Welcome to Python, ", name)
+
+number1 = input("Enter a number: ")
+number2 = input("Enter second number: ")
+number1 = int(number1)
+number2 = int(number2)
+
+
+print(number1 + number2)
